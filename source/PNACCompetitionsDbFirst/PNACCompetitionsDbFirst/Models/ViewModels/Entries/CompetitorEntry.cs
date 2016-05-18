b895810e086b2515e5ed8da75bf9b04b752d97ad
PedroArvy <petras.surna@gmail.com>
2016-05-18 21:54:51 +1000

    1

---
+++ source/PNACCompetitionsDbFirst/PNACCompetitionsDbFirst/Models/ViewModels/Entries/CompetitorEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PNACCompetitionsDbFirst.Models.ViewModels.Entries
+{
+  public class CompetitorEntry
+  {
+    public string Name { get; set; }
+
+    public int CompetitorId { get; set; }
+
+    public bool IsReferee { get; set; }
+
+    public bool IsTripCaptain { get; set; }
+  }
+}
+
+
